a437c3e3ab69eb2012e17aaa5d35fd43218f5554
Yann Howe <yannhowe@gmail.com>
2020-02-25 21:15:24 +0800

    derailed. lol.

---
+++ gitlab/gitlab-runner-register.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Get the registration token from:
+# http://localhost:8080/root/${project}/settings/ci_cd
+
+registration_token=a2bz9j3sNgkoUisHfzGx
+  
+docker exec -it gitlab-runner-1 \
+  gitlab-runner register \
+    --non-interactive \
+    --registration-token ${registration_token} \
+    --locked=false \
+    --description gitlab-runner-1 \
+    --url http://gitlab.platform.net:8080 \
+    --executor docker \
+    --docker-image docker:stable
