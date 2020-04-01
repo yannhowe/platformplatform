9a4f5e73064c434f3f12833df70d0a90e5ba9355
Yann Howe <yannhowe@gmail.com>
2020-04-01 16:51:50 +0800

    Added runners, registration scripts

--- platformplatform.sh
+++ platformplatform.sh
@@ -1,8 +1,39 @@
 #!/bin/bash
 
-DATE=`date +%Y-%m-%d-%H-%M-%S`
-
 case "$1" in
+        init)
+            if [ -f ./gitlab/.env ]
+            then
+                export $(cat ./gitlab/.env | xargs)
+            fi
+            docker-compose -f /home/platypus/Code/platformplatform/gitlab/docker-compose.yml exec gitlab-runner-1 \
+                gitlab-runner register \
+                    --non-interactive \
+                    --registration-token $REGISTRATION_TOKEN \
+                    --locked=false \
+                    --description gitlab-runner-1 \
+                    --url http://gitlab.platform.net:8088 \
+                    --executor docker \
+                    --docker-image docker:stable
+            docker-compose -f /home/platypus/Code/platformplatform/gitlab/docker-compose.yml exec gitlab-runner-2 \
+                gitlab-runner register \
+                    --non-interactive \
+                    --registration-token $REGISTRATION_TOKEN \
+                    --locked=false \
+                    --description gitlab-runner-2 \
+                    --url http://gitlab.platform.net:8088 \
+                    --executor docker \
+                    --docker-image docker:stable
+            docker-compose -f /home/platypus/Code/platformplatform/gitlab/docker-compose.yml exec gitlab-runner-3 \
+                gitlab-runner register \
+                    --non-interactive \
+                    --registration-token $REGISTRATION_TOKEN \
+                    --locked=false \
+                    --description gitlab-runner-3 \
+                    --url http://gitlab.platform.net:8088 \
+                    --executor docker \
+                    --docker-image docker:stable
+            ;;
         start)
             docker-compose -f /home/platypus/Code/platformplatform/chopchop/docker-compose.yml up -d
             docker-compose -f /home/platypus/Code/platformplatform/gitlab/docker-compose.yml up -d
