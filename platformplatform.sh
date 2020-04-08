69340008892e1ecfb5e9217ec46aeca6921d3ccb
Yann Howe <yannhowe@gmail.com>
2020-04-08 14:14:07 +0800

    add gitlab backup

--- platformplatform.sh
+++ platformplatform.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+wait_for_container_state () {
+    until [ `docker ps --filter "name=$1" --filter "health=$2" --format "{{.Names}}"` ]
+    do
+        docker ps --filter "name=$1" --format "{{.Names}} {{.Status}}"
+        sleep 5
+    done
+    docker ps --filter "name=$1" --format "{{.Names}} {{.Status}}"
+}
+
 case "$1" in
         init)
             if [ -f ./gitlab/.env ]
@@ -34,31 +43,58 @@ case "$1" in
                     --executor docker \
                     --docker-image docker:stable
             ;;
+        backup)
+            # GitLab
+            dt=$(date +'%s_%Y_%m_%d');
+            mkdir ${PWD}/gitlab/backup/secrets/$dt
+            docker cp platformplatform_gitlab_1:/etc/gitlab/ ${PWD}/gitlab/backup/secrets/$dt
+            docker exec -t platformplatform_gitlab_1 gitlab-backup create
+            ;;
+        backup_ls)
+            docker exec -it platformplatform_gitlab_1 ls /var/opt/gitlab/backups/
+            ;;
+        restore)
+            echo "Listing available backups:"
+            echo "=========================="
+            docker exec -it platformplatform_gitlab_1 ls /var/opt/gitlab/backups
+            echo "=========================="
+            echo ""
+            read -p 'Backup to restore: ' backup_to_restore
+            docker exec -it platformplatform_gitlab_1 gitlab-backup restore BACKUP=$backup_to_restore
+            
+            ;;
         up)
             docker-compose -f /home/platypus/Code/platformplatform/gitlab/docker-compose.yml up  -d
             docker-compose -f /home/platypus/Code/platformplatform/apt-mirror/docker-compose.yml up -d
+            docker-compose -f /home/platypus/Code/platformplatform/minio/docker-compose.yml up -d
             docker-compose -f /home/platypus/Code/platformplatform/chopchop/docker-compose.yml up -d
             docker-compose -f /home/platypus/Code/platformplatform/nginx/docker-compose.yml up -d
+            wait_for_container_state platformplatform_gitlab_1 healthy
             ;;
         down)
             docker-compose -f /home/platypus/Code/platformplatform/gitlab/docker-compose.yml down --remove-orphans
             docker-compose -f /home/platypus/Code/platformplatform/apt-mirror/docker-compose.yml down --remove-orphans
+            docker-compose -f /home/platypus/Code/platformplatform/minio/docker-compose.yml down --remove-orphans
             docker-compose -f /home/platypus/Code/platformplatform/chopchop/docker-compose.yml down --remove-orphans
             docker-compose -f /home/platypus/Code/platformplatform/nginx/docker-compose.yml down --remove-orphans
             ;;
         restart)
             docker-compose -f /home/platypus/Code/platformplatform/gitlab/docker-compose.yml down --remove-orphans
             docker-compose -f /home/platypus/Code/platformplatform/apt-mirror/docker-compose.yml down --remove-orphans
+            docker-compose -f /home/platypus/Code/platformplatform/minio/docker-compose.yml down --remove-orphans
             docker-compose -f /home/platypus/Code/platformplatform/chopchop/docker-compose.yml down --remove-orphans
             docker-compose -f /home/platypus/Code/platformplatform/nginx/docker-compose.yml down --remove-orphans
             docker-compose -f /home/platypus/Code/platformplatform/gitlab/docker-compose.yml up -d
             docker-compose -f /home/platypus/Code/platformplatform/apt-mirror/docker-compose.yml up -d
+            docker-compose -f /home/platypus/Code/platformplatform/minio/docker-compose.yml up -d
             docker-compose -f /home/platypus/Code/platformplatform/chopchop/docker-compose.yml up -d
             docker-compose -f /home/platypus/Code/platformplatform/nginx/docker-compose.yml up -d
+            wait_for_container_state platformplatform_gitlab_1 healthy
             ;;
         ps)
             docker-compose -f /home/platypus/Code/platformplatform/gitlab/docker-compose.yml ps
             docker-compose -f /home/platypus/Code/platformplatform/apt-mirror/docker-compose.yml ps
+            docker-compose -f /home/platypus/Code/platformplatform/minio/docker-compose.yml ps
             docker-compose -f /home/platypus/Code/platformplatform/chopchop/docker-compose.yml ps
             docker-compose -f /home/platypus/Code/platformplatform/nginx/docker-compose.yml ps
             ;;
