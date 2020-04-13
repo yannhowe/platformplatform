7fd1d411f54dc719198819a9ee0fa5f10805c112
plat <plat@plat.net>
2020-04-13 11:20:26 +0000

    :

--- scripts/upd_apt_platbundle.sh
+++ scripts/upd_apt_platbundle.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #exclude ubuntu updates
-sudo tar --exclude='/home/platypus/Code/platformplatform/apt-mirror/data' -czvf /upd02/platformbundle.zip  /home/platypus/Code/platformplatform/
+sudo tar --exclude='/home/platypus/Code/platformplatform/apt-mirror/data' -czvf /upd02/platformbundle.tar.gz  /home/platypus/Code/platformplatform/
 #exclude ubuntu updates
-sudo tar --exclude='/home/platypus/Code/platformplatform/apt-mirror/data' -czvf /mnt/pleco/minio-txout/platformbundle.zip  /home/platypus/Code/platformplatform/
+sudo tar --exclude='/home/platypus/Code/platformplatform/apt-mirror/data' -czvf /mnt/pleco/minio-txout/platformbundle.tar.gz  /home/platypus/Code/platformplatform/
 #Move from ext to dev
-cp -p /home/platypus/Code/platformplatform/apt-mirror/data /mnt/pleco/minio-tx
+sudo cp -rfp /home/platypus/Code/platformplatform/apt-mirror/data /mnt/pleco/minio-txout/ubuntu_data
 #rsync incremental from ext to internal
-sudo rsync -zarvh /home/platypus/Code/platformplatform/apt-mirror/data /upd02/
+sudo rsync -zarvh /home/platypus/Code/platformplatform/apt-mirror/data/* /upd02/ubuntu_data
