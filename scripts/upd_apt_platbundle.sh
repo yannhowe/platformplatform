a97b7bc4121c90cb9ffb3145162d38324dfc4a30
Desmond <60381871+goal86sg@users.noreply.github.com>
2020-04-14 10:53:37 +0800

    Update upd_apt_platbundle.sh

--- scripts/upd_apt_platbundle.sh
+++ scripts/upd_apt_platbundle.sh
@@ -4,6 +4,6 @@ sudo tar --exclude='/home/platypus/Code/platformplatform/apt-mirror/data' -czvf 
 #exclude ubuntu updates
 sudo tar --exclude='/home/platypus/Code/platformplatform/apt-mirror/data' -czvf /mnt/pleco/minio-txout/platformbundle.tar.gz  /home/platypus/Code/platformplatform/
 #Move from ext to dev
-sudo cp -rfp /home/platypus/Code/platformplatform/apt-mirror/data /mnt/pleco/minio-txout/ubuntu_data
+sudo rsync -zarvh /home/platypus/Code/platformplatform/apt-mirror/data/* /mnt/pleco/minio-txout/ubuntu_data
 #rsync incremental from ext to internal
 sudo rsync -zarvh /home/platypus/Code/platformplatform/apt-mirror/data/* /upd02/ubuntu_data
